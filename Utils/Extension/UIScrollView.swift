c4b9eb08a493335b635b3c490f6d36b13a3c4781
Tai Le <sirlevantai@gmail.com>
2015-10-30 22:55:08 +0700

    add scroll view extension

---
+++ Utils/Extension/UIScrollView.swift
@@ -0,0 +1,22 @@
+//
+//  UIScrollView.swift
+//  PinBike
+//
+//  Created by Le Tai on 10/30/15.
+//  Copyright © 2015 Le Van Tai. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+    func scrollToBottom(animated: Bool = true) {
+        func scrollToBottom() {
+            let bottomRect = CGRect(x: self.contentSize.width - 1, y: self.contentSize.height - 1, width: 1, height: 1)
+            self.scrollRectToVisible(bottomRect, animated: true)
+        }
+        
+        var isBouncing: Bool {
+            return contentOffset.y >= contentSize.height - bounds.size.height || contentOffset.y <= 0
+        }
+    }
+}
