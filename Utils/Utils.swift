4433c15ad00e179a0a5720e17f4a20021148939d
Tai Le <sirlevantai@gmail.com>
2015-11-03 22:51:36 +0700

    Check device screen

---
+++ Utils/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  Utils
+//
+//  Created by Le Tai on 11/3/15.
+//  Copyright © 2015 AJ. All rights reserved.
+//
+
+import UIKit
+
+public class Utils {
+    public var deviceLowerThanOrEqualIPhone4: Bool {
+        guard UIDevice.currentDevice().userInterfaceIdiom == .Phone && UIScreen.mainScreen().bounds.size.height < 568 else {
+            return false
+        }
+        return true
+    }
+}
