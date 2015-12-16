680ad099ae14b84887f8666f51b10db2c964cbff
Le Tai <sirlevantai@gmail.com>
2015-12-17 00:03:30 +0700

    Compare coordinate

--- Utils/Extension/CLLocationCoordinate2D.swift
+++ Utils/Extension/CLLocationCoordinate2D.swift
@@ -25,3 +25,10 @@ public extension CLLocationCoordinate2D {
         return Float(deg * (M_PI/180))
     }
 }
+
+extension CLLocationCoordinate2D: Equatable {}
+
+public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+}
+
