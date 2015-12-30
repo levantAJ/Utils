41d0f74a25a359c6caaac5f39a972c7cf3748385
Le Tai <sirlevantai@gmail.com>
2015-12-30 23:50:42 +0700

    App Error

--- Utils/Constants.swift
+++ Utils/Constants.swift
@@ -7,3 +7,97 @@
 //
 
 public struct Constants {}
+
+public enum MessageCode: Int {
+    case Unknown = 0
+    case Successfully = 1
+    case Fail = 2
+    case SystemException = 3
+    case InteractiveWithDatabaseFail = 4
+    case ElementIsNotFound = 5
+    case ElementIsInvalid = 6
+    case ElementIsEmpty = 7
+    case ElementHasBeenUsed = 8
+    case ElementIsInvalidOrUsed = 9
+    case ElementExpired = 10
+    case ObjectIsNotFound = 11
+    case ObjectIsNotBelongToYou = 12
+    case RequestHasAccepted = 13
+    case RequestHasDenied = 14
+    case CertificateVerified = 15
+    case UserIsBusy = 17
+    case UserAndAngelIsNotSamePlaceAndTime = 18
+    case NotificationRequestVerify = 500
+    case NotificationVerifyCertificateSuccess = 501
+    case NotificationRequestMakeTrip = 502
+    case NotificationReplyMakeTripAccept = 503
+    case NotificationReplyMakeTripDeny = 504
+    case NotificationRequestPhoneNumber = 505
+    case NotificationAcceptPhoneNumber = 506
+    case NotificationAlertTripComingUp = 507
+    case NotificationStartTrip = 508
+    case NotificationEndTrip = 509
+    
+    public var description: String {
+        switch self {
+        case .Unknown:
+            return NSLocalizedString("An unspecified error occurred", comment: "")
+        case .Successfully:
+            return NSLocalizedString("Successfully", comment: "")
+        case .Fail:
+            return NSLocalizedString("An unspecified error occurred", comment: "")
+        case .SystemException, .InteractiveWithDatabaseFail:
+            return NSLocalizedString("Internal Server Error", comment: "")
+        case .ElementIsEmpty:
+            return NSLocalizedString("There are some fields were empty", comment: "")
+        case .ElementExpired:
+            return NSLocalizedString("There are some fields were expired", comment: "")
+        case .ElementIsInvalid, .ElementIsInvalidOrUsed:
+            return NSLocalizedString("There are some field were invalid", comment: "")
+        case .ElementIsNotFound:
+            return NSLocalizedString("There are some fields were not found", comment: "")
+        case .ElementHasBeenUsed:
+            return NSLocalizedString("There are some fields have been used", comment: "")
+        case .ObjectIsNotFound:
+            return NSLocalizedString("Content not found", comment: "")
+        case .ObjectIsNotBelongToYou:
+            return NSLocalizedString("Content is not belong to you", comment: "")
+        case .RequestHasDenied:
+            return NSLocalizedString("Request has denied", comment: "")
+        case .RequestHasAccepted:
+            return NSLocalizedString("Request has accepted", comment: "")
+        case .CertificateVerified:
+            return NSLocalizedString("Certificate verified", comment: "")
+        case .UserIsBusy:
+            return NSLocalizedString("User is busy", comment: "")
+        case .UserAndAngelIsNotSamePlaceAndTime:
+            return NSLocalizedString("User and angel is not same place and time", comment: "")
+        case .NotificationRequestVerify:
+            return NSLocalizedString("Request verify", comment: "")
+        case .NotificationVerifyCertificateSuccess:
+            return NSLocalizedString("Verify certificates success", comment: "")
+        case .NotificationRequestMakeTrip:
+            return NSLocalizedString("Sent request make trip", comment: "")
+        case .NotificationReplyMakeTripAccept:
+            return NSLocalizedString("Your request make trip was accepted", comment: "")
+        case .NotificationReplyMakeTripDeny:
+            return NSLocalizedString("Your request make trip was denied", comment: "")
+        case .NotificationRequestPhoneNumber:
+            return NSLocalizedString("Wanna view your phone number", comment: "")
+        case .NotificationAcceptPhoneNumber:
+            return NSLocalizedString("Your request view phone number was accepted", comment: "")
+        case .NotificationAlertTripComingUp:
+            return NSLocalizedString("You have some coming trips", comment: "")
+        case .NotificationStartTrip:
+            return NSLocalizedString("Your trip begins", comment: "")
+        case .NotificationEndTrip:
+            return NSLocalizedString("Your trip ended", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.MessageCodeErrorDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
---
+++ Utils/Errors.swift
@@ -0,0 +1,222 @@
+//
+//  Errors.swift
+//  PinBike
+//
+//  Created by Le Tai on 10/16/15.
+//  Copyright © 2015 Le Van Tai. All rights reserved.
+//
+
+import UIKit
+
+public enum FailedMessage: Int, ErrorType, CustomStringConvertible {
+    case Failed
+    case DeleteTripFailed
+    case NoChangeWhenRepeatTrip
+    case RepeatTripFailed
+    
+    public var description: String {
+        switch self {
+        case .Failed:
+            return NSLocalizedString("Failed", comment: "")
+        case .DeleteTripFailed:
+            return NSLocalizedString("Delete the trip was failed", comment: "")
+        case .NoChangeWhenRepeatTrip:
+            return NSLocalizedString("Please change the time which you want to repeat the trip", comment: "")
+        case .RepeatTripFailed:
+            return NSLocalizedString("Could not repeate your trip", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.SuccessMessageDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public enum SuccessMessage: Int, ErrorType, CustomStringConvertible {
+    case Success
+    case DeleteTripSuccess
+    
+    public var description: String {
+        switch self {
+        case .Success:
+            return NSLocalizedString("Successfully", comment: "")
+        case .DeleteTripSuccess:
+            return NSLocalizedString("The trip has been successfully deleted", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.SuccessMessageDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public enum LoadingStatus: Int, ErrorType, CustomStringConvertible {
+    case Loading
+    case UpdateProfile
+    case MakeRequest
+    case RemoveRequest
+    case DeleteTrip
+    case FetchingFacebookInfo
+    case CreateTrip
+    case RetrievePairedTrips
+    case Rating
+    case LoadingPrice
+    case Sharing
+    
+    public var description: String {
+        switch self {
+        case .MakeRequest:
+            return NSLocalizedString("Making your request…", comment: "")
+        case .RemoveRequest:
+            return NSLocalizedString("Removing your request…", comment: "")
+        case .Loading:
+            return NSLocalizedString("Loading…", comment: "")
+        case .DeleteTrip:
+            return NSLocalizedString("Deleting…", comment: "")
+        case .FetchingFacebookInfo:
+            return NSLocalizedString("Fetching your Facebook info…", comment: "")
+        case .UpdateProfile:
+            return NSLocalizedString("Updating your profile…", comment: "")
+        case .CreateTrip:
+            return NSLocalizedString("We are posting your trip…", comment: "")
+        case .RetrievePairedTrips:
+            return NSLocalizedString("Retrieving paired trips…", comment: "")
+        case .Rating:
+            return NSLocalizedString("Rating…", comment: "")
+        case .LoadingPrice:
+            return NSLocalizedString("Calculating the price of the trip…", comment: "")
+        case .Sharing:
+            return NSLocalizedString("Sharing…", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.LoadingStatusDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public enum AlertError: Int, ErrorType, CustomStringConvertible {
+    case Location
+    case Failure
+    case CallFailure
+    
+    public var description: String {
+        switch self {
+        case .Location:
+            return Constants.Alert.locationFailureMessage
+        case .Failure:
+            return Constants.Alert.failureMessage
+        case .CallFailure:
+            return Constants.Alert.callFailureMessage
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.AlertErrorDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public enum AuthenticationError: Int, ErrorType, CustomStringConvertible {
+    case WrongPassword
+    case WrongEmail
+    case NotFound
+    case AccountExists
+    
+    public var description: String {
+        switch self {
+        case .NotFound, .WrongEmail:
+            return NSLocalizedString("The email you’ve entered doesn’t match any account", comment: "")
+        case .WrongPassword:
+            return NSLocalizedString("The password you’ve entered is incorrect", comment: "")
+        case .AccountExists:
+            return NSLocalizedString("Sorry, the email looks like belongs to an existing account", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.AuthenticationErrorDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public enum APIError: Int, ErrorType, CustomStringConvertible {
+    case Unspecified = 7000
+    case NoBodyFound = 7001
+    case Cached = 7002
+    case NoReceivers = 7003
+    case ChattingRealtime = 7004
+    case ChattingServer = 7005
+    case Location = 7006
+    
+    case Unauthorized = 401
+    case Forbidden = 403
+    case PageNotFound = 404
+    case TimeOut = 408
+    case InternalServer = 500
+    case Network = -1009
+    
+    public var description: String {
+        switch self {
+        case .NoBodyFound:
+            return NSLocalizedString("No content found", comment: "")
+        case .Unspecified:
+            return NSLocalizedString("An unspecified error occurred", comment: "")
+        case .Cached:
+            return NSLocalizedString("Can not access cache", comment: "")
+        case .PageNotFound:
+            return NSLocalizedString("Page Not Found", comment: "")
+        case .Unauthorized:
+            return NSLocalizedString("Authorization Required", comment: "")
+        case .Forbidden:
+            return NSLocalizedString("Forbidden", comment: "")
+        case .TimeOut:
+            return NSLocalizedString("Time Out", comment: "")
+        case .InternalServer:
+            return NSLocalizedString("Internal Server Error", comment: "")
+        case .Network:
+            return NSLocalizedString("Lost Internet Connection", comment: "")
+        case .NoReceivers:
+            return NSLocalizedString("No Receivers", comment: "")
+        case .ChattingServer:
+            return NSLocalizedString("Chatting Server Error", comment: "")
+        case .ChattingRealtime:
+            return NSLocalizedString("Your friend can not receive messages instantly", comment: "")
+        case .Location:
+            return NSLocalizedString("Could not get address, try again!", comment: "")
+        }
+    }
+    
+    public func foundationError() -> NSError {
+        return NSError(domain: Constants.Error.APIErrorDomain, code: rawValue, userInfo: [
+            NSLocalizedDescriptionKey: description
+            ])
+    }
+}
+
+public extension Constants {
+    public struct Error {
+        static let APIErrorDomain = "com.PinBike.Error.API"
+        static let AlertErrorDomain = "com.PinBike.Error.Alert"
+        static let LoadingStatusDomain = "com.PinBike.Error.Loading"
+        static let AuthenticationErrorDomain = "com.PinBike.Error.Authentication"
+        static let MessageCodeErrorDomain = "com.PinBike.Error.MessageCode"
+        static let SuccessMessageDomain = "com.PinBike.Error.SuccessMessage"
+        static let FailedMessageDomain = "com.PinBike.Error.FailedMessage"
+    }
+    
+    public struct Alert {
+        static let failureMessage = NSLocalizedString("Whoops! There are some problems were occurred, please try again!", comment: "")
+        static let locationFailureTitle = "Could not get your location"
+        static let locationFailureMessage = "Whoops! Please go to Setting and allow for us to access your loaction!"
+        static let callFailureMessage = NSLocalizedString("We could not call your partner, please check your phone and then call again!", comment: "")
+    }
+}
